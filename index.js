d1b2c6e6cbbea95c8a70e75b907d066b17f775c5
Kristina Diaz <kristina.diaz84@gmail.com>
2021-11-04 21:18:40 -0600

    commit tree 816720c149cfa98b3e32d5bd3c3963f2dbf3dc22

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+function findMatching(array, string) {
+    return array.filter((element) => {
+        return element.toLowerCase() === string.toLowerCase()
+    })
+}
+
+function fuzzyMatch(array, string) {
+    return array.filter((element) => {
+        return element.toLowerCase().indexOf(string.toLowerCase()) !== -1 && 
+        element.toLowerCase().indexOf(string.toLowerCase()) === 0;
+    })
+}
+
+function matchName(array, string) {
+    return array.filter((element) => {
+        return element.name === string;
+    })
+}
